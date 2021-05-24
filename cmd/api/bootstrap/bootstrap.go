6005453912d2cb402e8b432553bfedfa87657c4b
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-05-24 20:42:45 -0300

    feat(server): graceful shutdown

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"github.com/LuisCusihuaman/go-hexagonal-http-api/internal/creating"
@@ -8,11 +9,13 @@ import (
 	"github.com/LuisCusihuaman/go-hexagonal-http-api/internal/platform/server"
 	"github.com/LuisCusihuaman/go-hexagonal-http-api/internal/platform/storage/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 const (
-	host = "localhost"
-	port = 8080
+	host            = "localhost"
+	port            = 8080
+	shutdownTimeout = 10 * time.Second
 
 	dbUser = "codely"
 	dbPass = "codely"
@@ -38,6 +41,6 @@ func Run() error {
 
 	commandBus.Register(creating.CourseCommandType, createCourseCommandHandler)
 
-	srv := server.New(host, port, commandBus)
-	return srv.Run()
+	ctx, srv := server.New(context.Background(), host, port, shutdownTimeout, commandBus)
+	return srv.Run(ctx)
 }
--- internal/platform/server/server.go
+++ internal/platform/server/server.go
@@ -1,35 +1,67 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"github.com/LuisCusihuaman/go-hexagonal-http-api/kit/command"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/LuisCusihuaman/go-hexagonal-http-api/internal/platform/server/handler/courses"
 	"github.com/LuisCusihuaman/go-hexagonal-http-api/internal/platform/server/handler/health"
 	"github.com/gin-gonic/gin"
 )
 
+func serverContext(ctx context.Context) context.Context {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	ctx, cancel := context.WithCancel(ctx)
+	go func() { <-c; cancel() }()
+	return ctx
+}
+
 type Server struct {
 	httpAddr string
 	engine   *gin.Engine
+
+	shutdownTimeout time.Duration
+
 	// deps
 	commandBus command.Bus
 }
 
-func New(host string, port uint, commandBus command.Bus) Server {
+func New(ctx context.Context, host string, port uint, shutdownTimeout time.Duration, commandBus command.Bus) (context.Context, Server) {
 	srv := Server{
-		engine:     gin.New(),
-		httpAddr:   fmt.Sprintf("%s:%d", host, port),
-		commandBus: commandBus,
+		engine:          gin.New(),
+		httpAddr:        fmt.Sprintf("%s:%d", host, port),
+		shutdownTimeout: shutdownTimeout,
+		commandBus:      commandBus,
 	}
 	srv.registerRoutes()
-	return srv
+	return serverContext(ctx), srv
 }
 
-func (s *Server) Run() error {
+func (s *Server) Run(ctx context.Context) error {
 	log.Println("Server running on", s.httpAddr)
-	return s.engine.Run(s.httpAddr)
+
+	srv := &http.Server{
+		Addr:    s.httpAddr,
+		Handler: s.engine,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("server shut down", err)
+		}
+	}()
+	<-ctx.Done()
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+
+	return srv.Shutdown(ctxShutDown)
 }
 
 func (s *Server) registerRoutes() {
